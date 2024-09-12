9762ab9ce2421afaebf1cf3c968ec48c20530a56
feysel shemsu <feyselshemsu42@gmail.com>
2024-09-12 18:02:00 +0300

    future age calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,8 @@
 
-current_age = input("How lod are you?")
-age = int(current_age) + 27
-print("In 2050, you will be", age, "years old")
+current_age = int(input("How lod are you?"))
+if current_age > 0:
+    age = (current_age) + 27
+    print("In 2050, you will be", age, "years old")
+else:
+    print("please inter proper value")
+
