20190e4506c2a176c95cbc23352037802220d229
feysel shemsu <feyselshemsu42@gmail.com>
2024-09-18 10:43:38 +0300

    calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,13 +4,18 @@ operation = input("Choose the operation (+, -, *, /): ")
 match operation:
     case "+":
         result = num1 + num2
+        print(f"The result is {result}")
     case "-":
         result = num1 - num2
+        print(f"The result is {result}")
     case "*":
         result = num1 * num2
+        print(f"The result is {result}")
     case "/":
         if num2 == 0:
             result = str(print("Cannot divide by zero."))
+            print(result)
         else:
             result = num1 / num2
-print(result)
+            print(f"The result is {result}")
+
