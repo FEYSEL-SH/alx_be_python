db1d528aa04b952480b45d1f78cbed2487d5a90f
feysel shemsu <feyselshemsu42@gmail.com>
2024-09-28 09:24:02 +0300

    shopping list manager created

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,38 @@
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            addedItem = str(input("insert the item that you want to add"))
+            shopping_list.append(addedItem)
+            pass
+        elif choice == '2':
+            removed_item = str(input("Insert the item name to be removed: "))
+            if removed_item in shopping_list:
+                shopping_list.remove(removed_item)
+                print(f"{removed_item} has been removed from the shopping list.")
+            else:
+                print(f"{removed_item} is not in the shopping list.")
+            pass
+        elif choice == '3':
+
+            print(f"{shopping_list}")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
