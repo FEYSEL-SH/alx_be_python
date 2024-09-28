9b47803da998eb931abefae9a6aa42d71b06268c
feysel shemsu <feyselshemsu42@gmail.com>
2024-09-28 10:13:31 +0300

    explore_datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+import datetime
+from datetime import timedelta
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
+display_current_datetime()
+
+def calculate_future_date():
+    current_now = datetime.datetime.now()
+    addDate = int(input("input your data"))
+    delta = timedelta(
+    days=addDate,
+)
+    future_date = current_now + delta
+    print(future_date)
+calculate_future_date()
